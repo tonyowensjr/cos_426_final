59a671baec21ab7fc6cf7de4c60add8cebd3937f
Tony Owens <to5@princeton.edu>
2023-12-14 18:03:46 -0500

    config file added

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+
+export default defineConfig({
+    // Base URL for the application
+    base: '/cos_426_final/',
+    
+    // Output directory for the built files
+    build: {
+        outDir: 'dist',
+    },
+    
+    // Development server configuration
+    server: {
+        port: 3000,
+    },
+    
+    // Plugins and their configurations
+    plugins: [],
+});
+import { defineConfig } from 'vite';
