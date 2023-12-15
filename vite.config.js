a3ca51d99ca370aa7d9df1913b6c805120dfc368
Tony Owens <to5@princeton.edu>
2023-12-15 18:58:10 -0500

    deploy test 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-
+import { defineConfig } from 'vite';
 export default defineConfig({
     // Base URL for the application
     base: '/cos_426_final/',
-    
-    // Development server configuration
-    server: {
-        port: 3000,
-    },
-    
-    // Plugins and their configurations
-    plugins: [],
+
 });
-import { defineConfig } from 'vite';
+
