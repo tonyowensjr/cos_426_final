cb521786ea1cb0346c77ff9a997f52121280e687
Tony Owens <to5@princeton.edu>
2023-12-15 19:02:46 -0500

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     // Base URL for the application
     base: '/cos_426_final/',
+    homepage: "https://tonyowensjr.github.io/cos_426_final/",
+    scripts:
+    {
+        "predeploy": "npm run build",
+        "deploy": "gh-pages -d dist",
+        "dev": "vite",
+        "build": "vite build"
+    }
 
 });
 
